3a1becf79be623a8d66a8c5be0b288cc5e780738
justforstudys <justforstudys@163.com>
2019-08-29 23:27:07 +0800

    [init project 2]

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//引入element及其样式
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//引入css-reset
+import '@/assets/index.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/login/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="test">
+    <span class="test-span">{{title}}</span>
+    <el-button type="primary">确定</el-button>
+  </section>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      title: '测试代码'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .test {
+    .test-span {
+      font-size: 20px;
+    }
+  }
+</style>
